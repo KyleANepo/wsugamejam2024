57eed5a49ba00a2576d1b6d971ac45e481917a74
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2024-01-14 17:06:14 -0800

    Added level 4 and star system to most levels

--- nIcetime/Assets/WinScreen.cs
+++ nIcetime/Assets/WinScreen.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour
 {
     public GameObject winScreenUI;
+    public TimerDisplay td;
+    private int score;
+
+    public GameObject oneStar;
+    public GameObject twoStar;
+    public GameObject threeStar;
 
     public static bool GameWon = false;
 
@@ -26,6 +34,30 @@ public class WinScreen : MonoBehaviour
     {
         Time.timeScale = 0f;
         GameWon = true;
+        score = td.finalScore;
+        Stars(score);
+    }
+
+    public void Stars(int score)
+    {
+        if (score >= 988)
+        {
+            oneStar.SetActive(false);
+            twoStar.SetActive(false);
+            threeStar.SetActive(true);
+        }
+        else if (score >= 975)
+        {
+            oneStar.SetActive(false);
+            twoStar.SetActive(true);
+            threeStar.SetActive(false);
+        }
+        else
+        {
+            oneStar.SetActive(true);
+            twoStar.SetActive(false);
+            threeStar.SetActive(false);
+        }
     }
 
     public void nextLevel()
